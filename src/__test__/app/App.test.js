f0bd6465d727cfb35d4fe58d369e190dcc68779d
Dat Bac Do <dat.b.do@gmail.com>
2018-04-25 13:25:34 -0400

    fix some test

--- src/__test__/app/App.test.js
+++ src/__test__/app/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import store from "../../redux/store";
+
+import App from "../../app";
+
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>,
+        div
+    );
+    ReactDOM.unmountComponentAtNode(div);
 });
