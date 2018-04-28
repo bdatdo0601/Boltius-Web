044fb3f5cdf1fd9248b8244caede407d0edb7dcd
Dat Bac Do <dat.b.do@gmail.com>
2018-04-27 20:08:31 -0400

    figure out a way to pull news from wentworth. need to think about how to display

--- src/components/Home/NewsFeedCard/index.js
+++ src/components/Home/NewsFeedCard/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import { Card, Tabs } from "antd";
+import { Card, Tabs, List, Spin } from "antd";
+import InfiniteScroll from "react-infinite-scroller";
 
 import "./index.less";
 
@@ -11,11 +12,13 @@ class NewsFeedCard extends Component {
     static propTypes = {
         title: PropTypes.string,
         isMobile: PropTypes.bool,
+        // witNewsFeedData: PropTypes.array,
     };
 
     static defaultProps = {
         title: "What's going on?",
         isMobile: false,
+        // witNewsFeedData: [],
     };
 
     render() {
@@ -23,14 +26,39 @@ class NewsFeedCard extends Component {
             <Card className="news-feed-card" title={<h1 className="news-feed-title">{this.props.title}</h1>}>
                 <Tabs defaultActiveKey="1" tabPosition={this.props.isMobile ? "top" : "left"} style={{ height: 220 }}>
                     <TabPane tab="WIT News" key="1">
-                        WIT News
+                        {/* <InfiniteScroll
+                            initialLoad={false}
+                            pageStart={0}
+                            loadMore={this.handleInfiniteOnLoad}
+                            hasMore={!this.state.loading && this.state.hasMore}
+                            useWindow={false}
+                        >
+                            <List
+                                dataSource={this.state.data}
+                                renderItem={item => (
+                                    <List.Item key={item.id}>
+                                        <List.Item.Meta
+                                            title={<a href={item.link}>{item.title}</a>}
+                                            description={item}
+                                        />
+                                    </List.Item>
+                                )}
+                            >
+                                {this.state.loading &&
+                                    this.state.hasMore && (
+                                        <div className="demo-loading-container">
+                                            <Spin />
+                                        </div>
+                                    )}
+                            </List>
+                        </InfiniteScroll> */}
                     </TabPane>
-                    <TabPane tab="WSG News" key="2">
+                    {/* <TabPane tab="WSG News" key="2">
                         News from WSG
                     </TabPane>
                     <TabPane tab="Issues Tracker" key="3">
                         Issue Tables
-                    </TabPane>
+                    </TabPane> */}
                 </Tabs>
             </Card>
         );
